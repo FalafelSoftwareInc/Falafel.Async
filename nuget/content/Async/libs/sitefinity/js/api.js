d033ee64ef55363d822d28ae02337f4a18e2fecb
Basem Emara <Basem Emara>
2013-03-25 09:08:19 -0400

    Rearranged GitHub repository
    To make room for NuGet packaging and other non-deployable resources

---
+++ nuget/content/Async/libs/sitefinity/js/api.js
@@ -0,0 +1,21 @@
+﻿/**
+ * This is an API wrapper for Sitefinity
+ */
+define([
+	'jquery',
+    'utils/helpers'
+], function ($, Helpers) {
+
+    var getImagesByAlbum = function (name) {
+        return $.getJSON(Helpers.toServicesUrl('/images/getbyalbum?value=' + name));
+    }
+
+    var getImagesByAlbumId = function (id) {
+        return $.getJSON(Helpers.toServicesUrl('/images/getbyalbumid?id=' + id));
+    }
+
+    //PUBLIC PROPERTIES
+    return {
+        getImagesByAlbum: getImagesByAlbum
+    };
+});
