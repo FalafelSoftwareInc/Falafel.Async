762b10077c8996ec8d2bef43d17dd58c52363d89
Basem Emara <basememara@gmail.com>
2013-08-13 07:04:27 -0400

    Deployed to nuget

--- nuget/content/Async/utils/helpers.js
+++ nuget/content/Async/utils/helpers.js
@@ -294,6 +294,17 @@ define([
             }
         },
 
+        isCookiesEnabled: function () {
+            var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+            if (typeof navigator.cookieEnabled == 'undefined' && !cookieEnabled) {
+                document.cookie = 'testcookie';
+                cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
+            }
+
+            return cookieEnabled;
+        },
+
         pushUnique: function (arr, value) {
             if ($.inArray(value, arr) < 0)
                 arr.push(value);
